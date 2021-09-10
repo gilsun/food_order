80e7187d291ee1da80febe95ed54b6156f8c6d10
Haley <arsene77@gmail.com>
2021-09-09 18:23:27 -0700

    up to 136

---
+++ src/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (<section className={classes.summary}>
+    <h2>Delicious Food, Delivered To You</h2>
+    <p>
+      copy from resource
+    </p>
+  </section>);
+
+}
+
+export default MealsSummary;
